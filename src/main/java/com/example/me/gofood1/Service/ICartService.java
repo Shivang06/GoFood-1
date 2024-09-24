731d436407c55248ad166822d3ed4a1f7b2a1b2f
SGUPT114 <sgupt114@ford.com>
2024-09-24 19:03:58 +0530

    Added Cart Functionality

---
+++ src/main/java/com/example/me/gofood1/Service/ICartService.java
@@ -0,0 +1,14 @@
+package com.example.me.gofood1.Service;
+
+import com.example.me.gofood1.Model.Cart;
+
+import java.util.List;
+
+public interface ICartService {
+
+    public List<Cart> getCart();
+    public void addCart(Cart cart);
+    public boolean deleteFoodItem(Cart cart);
+    public void increaseQuantity(Cart cart);
+    public void decreaseQuantity(Cart cart);
+}
