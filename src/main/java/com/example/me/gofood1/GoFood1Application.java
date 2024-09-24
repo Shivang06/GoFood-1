d3969a01ec25d4ef4b18de9d708cd027412126f9
SGUPT114 <sgupt114@ford.com>
2024-09-24 15:24:29 +0530

    Initial commit

---
+++ src/main/java/com/example/me/gofood1/GoFood1Application.java
@@ -0,0 +1,13 @@
+package com.example.me.gofood1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFood1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoFood1Application.class, args);
+    }
+
+}
