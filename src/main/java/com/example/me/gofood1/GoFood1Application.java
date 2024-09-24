f0ed7045bd4ceb928a5c2c6988f190e4ddfd3e46
SGUPT114 <sgupt114@ford.com>
2024-09-24 15:37:18 +0530

    Config

--- src/main/java/com/example/me/gofood1/GoFood1Application.java
+++ src/main/java/com/example/me/gofood1/GoFood1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoFood1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GoFood1Application.class, args);
     }
 
