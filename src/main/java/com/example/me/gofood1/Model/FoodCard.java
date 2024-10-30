0ed17546aa8c967f813f74b593315c6c5c3926f3
PDEWANGA <PDEWANGA@ford.com>
2024-10-30 17:18:05 +0530

    Updated Food Card

--- src/main/java/com/example/me/gofood1/Model/FoodCard.java
+++ src/main/java/com/example/me/gofood1/Model/FoodCard.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +18,8 @@ import lombok.NoArgsConstructor;
 public class FoodCard {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "foodcard_seq")
+    @SequenceGenerator(name = "foodcard_seq", sequenceName = "foodcard_sequence", allocationSize = 1)
     private int id;
     private String name;
     private double price;
