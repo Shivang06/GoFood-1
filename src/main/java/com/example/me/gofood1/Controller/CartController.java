b28d9ac37bbc5b75eb74d10be6687e6ccf0ee99e
Aakash <aakashv72003@gmail.com>
2024-11-13 23:03:06 +0530

    Cross Origin Local Host Updated at Cart Controller

--- src/main/java/com/example/me/gofood1/Controller/CartController.java
+++ src/main/java/com/example/me/gofood1/Controller/CartController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CartController {
 
     @Autowired
